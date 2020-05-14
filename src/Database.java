0a3333e90f89e8b34cb4e382774fd609213bb846
kyakmehmet <65353042+kyakmehmet@users.noreply.github.com>
2020-05-14 17:46:28 +0300

    Add files via upload

---
+++ src/Database.java
@@ -0,0 +1,11 @@
+
+public class Database {
+    static final String kullanici_adi = "root";
+    static final String parola = "";
+    static final String db_ismi = "java_calisan_db";
+    static final String host = "localhost";
+    
+    
+    static final int port = 3306;
+     
+}
